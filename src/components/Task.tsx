75ee13927276f2a121587f0935bab1622d401bc1
Stranger667 <88589797+StrangerZ667@users.noreply.github.com>
2024-07-25 14:17:57 +0700

    Update Task.tsx

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -24,10 +24,9 @@ export default function Task({
 
   return (
     <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-      {
-        <span className="text-decoration-line-through">{title}</span>
-      }
-      <span>{title}</span>
+      {completed ? 
+        (<span className="text-decoration-line-through">{title}</span>)
+      : (<span>{title}</span>)}
       <button className="btn btn-success" onClick={doneBtnOnClick}>Done</button>
       <button className="btn btn-danger" onClick={deleteBtnOnClick}>Delete</button>
     </div>
